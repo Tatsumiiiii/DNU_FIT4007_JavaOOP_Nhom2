98bd5c8384c5d7f00ef2de02fff3a34e925c993a
panhyeuoi <panhciuvl8126@gmail.com>
2025-11-15 12:57:28 +0700

    update

--- src/Main.java
+++ src/Main.java
@@ -11,6 +11,7 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Main {
-
+    private static HotelManager manager;
+    private static Scanner scanner = new Scanner(System.in);
 
 }
