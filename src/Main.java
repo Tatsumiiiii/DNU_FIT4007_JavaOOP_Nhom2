0032b96ae6331ac6ee8633456d95c986d10e71cf
HungVu646 <vkhung20062006@example.com>
2025-11-16 19:49:03 +0700

    update domain invoice

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,23 @@
-public class Main {
+package hotel;
 
+import hotel.domain.HotelManager;
+import hotel.exceptionHandling.*;
+import hotel.model.*;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+    private static HotelManager manager;
+    private static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        manager = new HotelManager();
+        boolean running = true;
+        while (running) {
+            showMenu();
+            int choice = getIntInput("Chọn chức năng: ");
 }
